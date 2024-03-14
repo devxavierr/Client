9bac64042c83b074ec269f31c7cd80ef22e8b2e2
devxavierr <devxavierr@gmail.com>
2024-03-14 16:50:50 -0300

    git init

---
+++ src/test/java/com/xavier/clients/ClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavier.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
