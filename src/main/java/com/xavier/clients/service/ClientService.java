f5dfc3ad94f0b3f4e36ea264313dae8aeaa46f38
devxavierr <devxavierr@gmail.com>
2024-03-16 18:47:15 -0300

    implements metodo findAll Client

--- src/main/java/com/xavier/clients/service/ClientService.java
+++ src/main/java/com/xavier/clients/service/ClientService.java
@@ -5,6 +5,8 @@ import com.xavier.clients.dto.ClientDTO;
 import com.xavier.clients.repository.ClientRepository;
 import com.xavier.clients.service.exception.ResouceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +21,11 @@ public class ClientService {
         Client client = repository.findById(id).orElseThrow(
                 ()-> new ResouceNotFoundException("Recurso não encontrado"));
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> client = repository.findAll(pageable);
+        return client.map(x-> new ClientDTO(x));
     }
 }
